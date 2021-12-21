ae4471be4797788f7b57575804fa08ae4eccbb55
andrew <andrew@io.shawtonabbey.com>
2021-12-21 12:23:36 -0600

    view refresh

--- src/com/shawtonabbey/pgem/plugin/refresh/RefreshPlugin.java
+++ src/com/shawtonabbey/pgem/plugin/refresh/RefreshPlugin.java
@@ -7,6 +7,7 @@ import com.shawtonabbey.pgem.event.EventDispatch;
 import com.shawtonabbey.pgem.plugin.Plugin;
 import com.shawtonabbey.pgem.tree.Event;
 import com.shawtonabbey.pgem.tree.table.TableGroup;
+import com.shawtonabbey.pgem.tree.view.ViewGroup;
 
 @Component
 public class RefreshPlugin implements Plugin {
@@ -22,13 +23,22 @@ public class RefreshPlugin implements Plugin {
 	public void init() {
 				
 		
-		dispatch.find(TableGroup.Ev.class).listen((m,ev) -> {
+		dispatch.find(ViewGroup.Ev.class).listen((m,ev) -> {
 			
 			m.addPopup("Refresh", (e) -> {
-
+				
 				var event = new Event();
 				m.refresh(event);
 				
+			});
+		});
+		
+		dispatch.find(TableGroup.Ev.class).listen((m,ev) -> {
+			
+			m.addPopup("Refresh", (e) -> {
+				
+				var event = new Event();
+				m.refresh(event);
 				
 			});
 		});
