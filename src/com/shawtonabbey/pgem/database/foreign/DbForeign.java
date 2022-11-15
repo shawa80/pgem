07a73f224c08b0b0539905b2c083162041988f30
andrew <andrew@io.shawtonabbey.com>
2022-11-14 22:03:52 -0600

    foriegn key stuff

--- src/com/shawtonabbey/pgem/database/foreign/DbForeign.java
+++ src/com/shawtonabbey/pgem/database/foreign/DbForeign.java
@@ -9,4 +9,22 @@ public class DbForeign {
 	@Getter
 	private String name;
 	
+	@Getter
+	private String primary_schema;
+	
+	@Getter 
+	private String primary_table;
+	
+	@Getter
+	private String primary_column;
+	
+	@Getter
+	private String foreign_table_schema;
+	
+	@Getter
+	private String foreign_table_name;
+	
+	@Getter
+	private String foreign_column_name;
+	
 }
--- src/com/shawtonabbey/pgem/database/foreign/DbForeignFactory.java
+++ src/com/shawtonabbey/pgem/database/foreign/DbForeignFactory.java
@@ -16,15 +16,32 @@ public class DbForeignFactory {
 
 	public List<DbForeign> getForeign(DBC connection, DbTable table) throws IOException {
 		
-		var sqlStr = "SELECT " +
-				"       conname AS foreign_key " + 
-				"FROM   pg_constraint " + 
-				"WHERE  contype = 'f' " + 
-				"AND    connamespace = 'public'::regnamespace   " + 
-				"ORDER  BY conrelid::regclass::text, contype DESC;";
+//		var sqlStr = "SELECT " +
+//				"       conname AS foreign_key " + 
+//				"FROM   pg_constraint " + 
+//				"WHERE  contype = 'f' " + 
+//				"AND    connamespace = 'public'::regnamespace   " + 
+//				"ORDER  BY conrelid::regclass::text, contype DESC;";
 
+		var sqlStr = "SELECT " + 
+			"    tc.constraint_name as name, " + 
+			"    tc.table_schema as primary_schema, " + 
+			"    tc.table_name as primary_table, " + 
+			"    kcu.column_name as primary_column, " + 
+			"    ccu.table_schema AS foreign_table_schema," + 
+			"    ccu.table_name AS foreign_table_name," + 
+			"    ccu.column_name AS foreign_column_name " + 
+			"FROM " + 
+			"    information_schema.table_constraints AS tc " + 
+			"    JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema " + 
+			"    JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name AND ccu.table_schema = tc.table_schema " + 
+			"WHERE tc.constraint_type = 'FOREIGN KEY' " + 
+			"AND ccu.table_schema = ? " +
+			"AND ccu.table_name = ?;";
+		
+		
 		var c = new Constr<>(DbForeign.class);
-		var results = connection.execX(sqlStr, c);
+		var results = connection.execX(sqlStr, c, table.getSchema().getName(), table.getName());
 		
 		return results;
 	}
